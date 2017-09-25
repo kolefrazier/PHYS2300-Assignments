472092f5ab532b0f9e83b34ccba010ed1dc11076
Kole <kolefrazier@mail.weber.edu>
2017-09-24 22:24:59 -0600

    Completed 3.1

--- Assignments/Assignment 3/Exercise 3.1.py
+++ Assignments/Assignment 3/Exercise 3.1.py
@@ -1,15 +1,48 @@
 #Exercise 3.1 - Plotting experimental data
 
-from pylab import plot,show
-from math import sin
-from numpy import linspace
+import pylab as plt
 
-xpoints = []
-ypoints = []
+Sunspots = []
+Months = []
+RunningAverages = []
 
-for x in linspace(0,10,100):
-    xpoints.append(x)
-    ypoints.append(sin(x))
+filename = "sunspots.txt"
 
-plot(xpoints,ypoints)
-show()
+try:
+    datafile = open(filename, 'r')
+    
+    for line in datafile:
+        try:
+            data = line.split('\t')
+            #[0] = Month, [1] = Sunspot number
+            Months.append(data[0])
+            Sunspots.append(data[1])
+            
+            SunspotSum = 0.0
+            EntryCounter = 0
+            for spot in Sunspots:
+                SunspotSum += float(spot)
+                EntryCounter += 1
+                
+            RunningAverages.append(SunspotSum/EntryCounter)
+            
+            if(len(Sunspots) >= 1000):
+                break;
+        except Exception as e:
+            print('(file io) Error {0}'.format(e))
+except Exception as e:
+    print('(main) Error {0}'.format(e))
+    
+#print('Months: {0}\tSunspots: {1}\tAves: {2}'.format(len(Months), len(Sunspots), len(RunningAverages)))
+
+HighestMonth = 0
+for m in Months:
+    if m > HighestMonth:
+        HighestMonth = m
+print('HighestMonth: ' + HighestMonth)
+
+plt.plot(Months, Sunspots, 'bo')
+plt.plot(Months, RunningAverages, 'yo')
+plt.xlabel('Months since 1749')
+plt.ylabel('Sunspots (Blue), Running Average (Yellow)')
+plt.show()
