8f1997c0fe02c8776d0137c0427780ef48389e9d
Unknown <kolefrazier@mail.weber.edu>
2017-10-23 20:16:27 -0600

    Setup base file for 5.2

---
+++ Assignments/Assignment 5/Exercise 5.2.py
@@ -0,0 +1,27 @@
+#--------------------------------------------------------------------------------
+# G e n e r a l I n f o r m a t i o n
+#--------------------------------------------------------------------------------
+# Name: Exercise 5.2
+#
+# Usage: python "Exercise 5.2.py"
+#
+# Description: Calculate and plot distance traveled using given velocity and time data.
+#       Uses trapezoid rule.
+#
+# Inputs: None
+#
+# Outputs: Console data AND a visual plot
+#
+# Auxiliary Files: velocity.txt
+#
+# Special Instructions: Script will output data into the console and display a visual plot - watch for both!
+#
+#--------------------------------------------------------------------------------
+# C o d e H i s t o r y
+#--------------------------------------------------------------------------------
+# Version: 1.0
+#
+# Author(s): Kole Frazier
+#
+#--------------------------------------------------------------------------------
+
