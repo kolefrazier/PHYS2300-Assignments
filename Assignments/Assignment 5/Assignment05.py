050fde9aec20a5b17f2d29f085f2f3775793e41f
Kole <kolefrazier@mail.weber.edu>
2017-10-25 15:11:52 -0600

    Work on A05, updated TODO with some notes

--- Assignments/Assignment 5/Assignment05.py
+++ Assignments/Assignment 5/Assignment05.py
@@ -1,6 +1,25 @@
 #Assignment05
 from visual import *
 import math as m
+import numpy
+
+def acceleration(F, m):
+    return (F/m)
+
+def velocity(v0, a, t0, t):
+    #v=v0+a*DeltaT
+    return (v0 + a*(t-t0))
+
+def position(x0, v, t0, t):
+    #x = x0 + v*DeltaT
+    return (x0 + v*(t-t0))
+
+def airResistance(v, A):
+    p = 0.0 #air density
+    cd = 0.5 #drag coefficient, 0.5 is a recommended estimate
+    F = -(1.0/2.0)*p*v*cd*A*v
+
+    return F
 
 #Get user inputs
 print('--- Initial Positional Values ---')
@@ -16,7 +35,7 @@ vz = float(input('Enter initial VZ: '))
 
 print('--- Other Values ---')
 timeStep = float(input('Enter time step: '))
-timeRun = float(input('Enter simulation run length: '))
+timeRun = int(input('Enter simulation run length (whole seconds): '))
 
 #Establish other values
 gravitationalAcceleration = vector(0, -9.8, 0)
@@ -29,8 +48,18 @@ ballNoDrag.velocity = vector(vx, vy, vz)
 ballAirDrag = sphere(pos = (x,y,z), radius=0.5, color=color.blue)
 ballAirDrag.velocity = vector(vx, vy, vz)
 
-for t in range(0, timeRun, timeStep):
+t0 = 0
+for t in numpy.arange(0, timeRun, timeStep):
     rate(5000)
     
     ballNoDrag.velocity = ballNoDrag.velocity + gravitationalAcceleration * timeStep
     ballNoDrag.pos = ballNoDrag.pos = ballNoDrag.velocity * timeStep
+
+#def position(x0, v, t0, t):
+    if(ballNoDrag.pos.y > 0):
+        ballNoDrag.pos.x = position(ballNoDrag.pos.x, ballNoDrag.velocity.x, t0, t)
+        ballNoDrag.pos.y = position(ballNoDrag.pos.y, ballNoDrag.velocity.y, t0, t)
+        ballNoDrag.pos.z = position(ballNoDrag.pos.z, ballNoDrag.velocity.z, t0, t)
+
+    #Store the previous time to assist in calculating DeltaT.
+    t0 = t
