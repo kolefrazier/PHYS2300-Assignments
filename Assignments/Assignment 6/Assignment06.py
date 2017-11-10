fdac5e67f1579d62f648c28eaf9926864120c6da
Kole F <kolefrazier@mail.weber.edu>
2017-11-09 21:21:54 -0700

    Work on A06.py
    Stupid thing actually dampens now. Only had to follow two existing examples.....

--- Assignments/Assignment 6/Assignment06.py
+++ Assignments/Assignment 6/Assignment06.py
@@ -24,37 +24,84 @@
 #--------------------------------------------------------------------------------
 
 from visual import *
-from numpy import *
-
-def getTorque(theta):
-    return -1.0 * L * m * g * sin(theta)
+from numpy import sin, cos
 
 def momentInertia(m, L): #I
     return m*(L**2)
 
-def angularAcceleration(theta)
+def angularAcceleration(theta): #alpha
+    return -g/L*sin(theta)
 
-def damping(theta):
-    reutnr -1*(g/L)*sin(theta)-
+def getDampedOn(a, w):
+    return a-(c*w)
+
+def angularVelocity(a, t): #w=w0+angularAccel*t => w += (a*t)
+    return a*t
+
+def DThetaDT(theta, t):
+    acceleration = angularAcceleration(theta)
+    dampedVelocity = angularVelocity(w0, a, t) #* c
+    frequency = A*sin(f*t)
+    return accel - dampedVelocity + frequency
+
+#def rk4():
+    
 
 #Simulation constants
-g = 9.8     #Gravity
-L = 1.0     #Bar length, meters
-DBar = 0.1  #Bar diameter
-DBall = 0.5 #Ball diameter
-MBall = 3.0 #Ball mass
-A = 
+g = 9.8         #Gravity
+L = 1.0         #Bar length, meters
+DBar = 0.05     #Bar diameter
+DBall = 0.5     #Ball diameter
+MBall = 3.0     #Ball mass
+A = .1          #Natural Frequency Coefficient (?? - Just given as a variable in the text)
+f = 2.0/3.0     #Frequency
+c = 1.0         #Damping Coefficient
+theta = 2.0 * pi / 3.0  #Starting Theta
+Velocity = 0.0          #Starting Velocity
 
-c = 1.0
-w = 1.0
-
-#Positional Vals
+#Initial Positional Values
 x = 0
 y = 0
 z = 0
 
-#Setup Shapes
-bar = cylinder(pos=vector(0,0,0), radius = DBar, color=color.yellow)
+#Time
+TStart = 0
+TEnd = 50
+TStep = 0.01
 
-while True:
+xPoints = []
+yPoints = []
+zPoints = []
+vPoints = []
+tPoints = arange(TStart, TEnd, TStep)
+
+#Setup Shapes
+bar = cylinder(pos=array([0,0,0]), radius = DBar, color=color.yellow)
+bar.axis = (L*sin(theta), -L*cos(theta),0)
+
+for t in tPoints:
     rate(60)
+
+    #Log current coordinates
+    xPos = L*sin(theta)
+    yPos = -L*cos(theta)
+    zPos = 0
+    xPoints.append(xPos)
+    yPoints.append(yPos)
+    zPoints.append(zPos)
+
+    #Calculate new acceleration, apply damping
+    newAccel = angularAcceleration(theta)
+    newAccel -= c*Velocity
+
+    #Calculate velocity
+    Velocity += (newAccel * TStep) #V = V0 + at => v += at
+    
+    #Calculate new Theta
+    theta += Velocity * TStep
+
+    #Update pendulum visual position
+    bar.axis = (L*sin(theta), -L*cos(theta),0)
+
+
+
