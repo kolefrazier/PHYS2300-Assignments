73fed7c0f09ed1e550783861d2c0142f77d6582b
Kole Frazier <kolefrazier@mail.weber.edu>
2017-11-13 15:07:58 -0700

    Work on Assignment 07

---
+++ Assignments/Assignment 7/Assignment07.py
@@ -0,0 +1,24 @@
+from visual import *
+
+def Force(m1, m2, R1, R2):
+    R12 = RDelta(R1, R2)
+    return (G*m1*m2)/RAbs(R1, R2)**2 * R12
+
+def RDelta(R1, R2):
+    return R2 - R1
+
+def RAbs(R1, R2):
+    deltaX = R2.x - R1.x
+    deltaY = R2.y - R1.y
+    deltaZ = R2.z - R1.z
+    PreSquareRoot = deltaX**2 + deltaY**2 + deltaZ**2
+    return (PreSquareRoot)**(1/2)
+
+def VDelta(a, t0, t):
+    return a * (t - t0)
+
+def Acceleration(m, R1, R2):
+    G * m / RAbs(R1, R2)**3 * 
+
+#Constants
+G = 1.0
