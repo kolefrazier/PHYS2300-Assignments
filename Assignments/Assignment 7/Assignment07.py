adf20af2a01aaf57ac9acc9177ac5da4cfae230f
Kole Frazier <kolefrazier@mail.weber.edu>
2017-11-15 15:15:38 -0700

    Minor updates to Assignment07 and Notes

--- Assignments/Assignment 7/Assignment07.py
+++ Assignments/Assignment 7/Assignment07.py
@@ -5,7 +5,11 @@ def Force(m1, m2, R1, R2):
     return (G*m1*m2)/RAbs(R1, R2)**2 * R12
 
 def RDelta(R1, R2):
-    return R2 - R1
+    deltaX = R2.x - R1.x
+    deltaY = R2.y - R1.y
+    deltaZ = R2.z - R1.z
+    returnVector = vector(deltaX, deltaY, deltaZ)
+    return returnVector
 
 def RAbs(R1, R2):
     deltaX = R2.x - R1.x
@@ -18,7 +22,13 @@ def VDelta(a, t0, t):
     return a * (t - t0)
 
 def Acceleration(m, R1, R2):
-    G * m / RAbs(R1, R2)**3 * 
+    return (G * m / RAbs(R1, R2)**3) * RDelta(R1, R2)
+
+
 
 #Constants
 G = 1.0
+
+PlanetRadius = 6
+StarRadius = 100
+RadiusScale = 10
