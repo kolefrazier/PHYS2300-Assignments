e756fe4dbce4a04fb1e07ab5978af82993c2d150
Kole Frazier <kolefrazier@mail.weber.edu>
2017-11-18 16:31:57 -0700

    Minor updates to Assignment07

--- Assignments/Assignment 7/Assignment07.py
+++ Assignments/Assignment 7/Assignment07.py
@@ -1,5 +1,7 @@
 from visual import *
 
+# ---------- Calculation Functions ----------
+
 def Force(m1, m2, R1, R2):
     R12 = RDelta(R1, R2)
     return (G*m1*m2)/RAbs(R1, R2)**2 * R12
@@ -21,10 +23,26 @@ def RAbs(R1, R2):
 def VDelta(a, t0, t):
     return a * (t - t0)
 
+def XDelta (v, t0, t):
+    return v * (t - t0)
+
 def Acceleration(m, R1, R2):
     return (G * m / RAbs(R1, R2)**3) * RDelta(R1, R2)
 
+def VInitial(m1, R1, R2, t, t0):
+    a = Acceleration(m1, R1, R2)
+    v = v
 
+# ---------- User Input ----------
+Mass1 = float(input('Enter Mass 1: '))
+Position1 = vector(float(input('Enter X-Pos 1: ')), float(input('Enter Y-Pos 1: ')), float(input('Enter Z-Pos 1: ')))
+Velocity1 = vector(float(input('Enter X-Velocity 1: ')), float(input('Enter Y-Velocity 1: ')), float(input('Enter Z-Velocity 1: ')))
+
+Mass2 = float(input('Enter Mass 2: '))
+Position2 = vector(float(input('Enter X-Pos 2: ')), float(input('Enter Y-Pos 2: ')), float(input('Enter Z-Pos 2: ')))
+Velocity2 = vector(float(input('Enter X-Velocity 2: ')), float(input('Enter Y-Velocity 2: ')), float(input('Enter Z-Velocity 2: ')))
+
+Masses = [Mass1, Mass2]
 
 #Constants
 G = 1.0
@@ -32,3 +50,11 @@ G = 1.0
 PlanetRadius = 6
 StarRadius = 100
 RadiusScale = 10
+
+#Objects/Bodies
+Object1 = ball(pos=Position1, velocity=
+objects = []
+
+#Main loop
+for o in objects:
+    print(str(o))
