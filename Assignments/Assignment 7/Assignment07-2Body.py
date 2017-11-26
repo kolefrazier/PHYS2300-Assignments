4a6c9250eb1836cd90287dbac89cb0ed530d5244
Kole F <kolefrazier@mail.weber.edu>
2017-11-26 14:22:14 -0700

    Suddenly, the 2Body sim works just fine... huh.

--- Assignments/Assignment 7/Assignment07-2Body.py
+++ Assignments/Assignment 7/Assignment07-2Body.py
@@ -4,25 +4,25 @@ from visual import *
 
 # ---------- Body Class ----------
 #Class: Body extends vpython's sphere
-#   Assists in data containment for ease in data management,
-#     because there is no way in hell that managing multiple
-#     arrays of associated data is reasonable.
+#   Assists in data containment for ease in data management.
+#   Managing multiple arrays of associated data manually just isn't
+#     good practice when concepts to handle associated data - such as OOP - exist.
 class Body(sphere):
-    def __init__(self, name='', mass=1.1, radius=10.0, velocity=vector(0.0, 0.0, 0.0), acceleration=vector(0.0, 0.0, 0.0), position=vector(1.0, 1.0, 1.0), color=color.white):
+    def __init__(self, name='', mass=1.1, radius=10.0, velocity=vector(0.0, 0.0, 0.0), acceleration=vector(0.0, 0.0, 0.0), position=vector(1.0, 1.0, 1.0), color=color.white, makeTrail=True, trailRetain=1500):
         self.name = name
         self.mass = mass
         self.acceleration = acceleration
-        sphere.__init__(self, radius=radius, pos=position, velocity=velocity, color=color, make_trail=True, retain=5000)
+        sphere.__init__(self, radius=radius, pos=position, velocity=velocity, color=color, make_trail=makeTrail, retain=trailRetain)
 
     def PrintDetails(self):
         print '[{0}]\n\tMass: {1}\n\tAccel: {2}\n\tVeloc: {3}\n\tpos: {4}'.format(self.name, str(self.mass), str(self.acceleration), str(self.velocity), str(self.pos))
 
 # ---------- Body Instantiation Functions ----------
 def GetEarth():
-    return Body(name='Earth', radius=10, mass=5.97e24, velocity=vector(0.0, 3.0e4, 0.0), position=vector(0.0, 0.0, 0.0), color=BodyColors['Earth+Moon barycenter'])
+    return Body(name='Earth', radius=10000, mass=5.97e24, velocity=vector(0.0, 3.0e4, 0.0), position=vector(1.5e11, 0.0, 0.0), color=BodyColors['Earth+Moon barycenter'])
 
 def GetSun():
-    return Body(name='Sun', radius=10, mass=1.99e30, velocity=vector(0.0, 0.0, 0.0), position=vector(0.0, 0.0, 0.0), color=BodyColors['Sun'])
+    return Body(name='Sun', radius=10000000, mass=1.99e30, velocity=vector(0.0, 0.0, 0.0), position=vector(0.0, 0.0, 0.0), color=BodyColors['Sun'], trailRetain=5000)
 
 # ---------- Numerical and Other Constants ----------
 G = 6.67e-11
@@ -35,30 +35,26 @@ Bodies = [GetSun(), GetEarth()]
 
 # ---------- Other VPython and Main-Loop Stuff ----------
 scene = display(title='2-Body Simulation', width=600, height=500, visible=True, show_rendertime=True, autoscale=False)
-RunTime = 3.15e7
-TimeChange = 6.3e4
-CurrentTime = 0
+RunTime = 3.15e7 * 3
+dt = 6.3e4
+CurrentTime = 0.0
+print('Beginning simulation. You may need to zoom in (middle house button) to see the some trails (such as the Sun\'s trail)')
 
 # ---------- Simulation Loop ----------
 while(CurrentTime <= RunTime):
-#for dt in arange(0, RunTime, TimeStep):
-#for dt in np.linspace(0, RunTime, TimeChange):
     rate(3600)
-
-    if CurrentTime == 0:
-        dt = 0
-    else:
-        dt = TimeChange
     
     for i in Bodies:
         i.acceleration = vector(0,0,0)
         for j in Bodies:
             if i != j:
                 dist = j.pos - i.pos
-                i.acceleration = i.acceleration + G * j.mass * dist / mag(dist)**3
+                i.acceleration += G * j.mass * dist / mag(dist)**3 
         for i in Bodies:
-            i.velocity = i.velocity + i.acceleration * dt
-            i.pos = i.pos + i.velocity * dt
+            i.velocity += i.acceleration * dt   #deltaV1 = A1 * deltaT
+            i.pos += i.velocity * dt            #deltaX1 = V1 * deltaT
+     
+    CurrentTime += dt
 
 print('--- Finished! ---')
 
