ce27ea576ef2c8eb5bed16631083b43d6654b674
Kole F <kolefrazier@mail.weber.edu>
2017-12-12 16:09:06 -0700

    Lots more work

--- Assignments/Final Project/FinalProject-VSVersion.py
+++ Assignments/Final Project/FinalProject-VSVersion.py
@@ -9,28 +9,34 @@ import numpy as np
 # File IO, Raw Data Functions
 #--------------------------------------------------------------------------------
 
+#Method to read in all lines from given data file at once.
+#   It reads them all in at once to speed up processing later.
+#   (A read->process->read->process would be terribly inefficient.)
 def ReadDataFile(FileName):
     FileHandle = open(FileName)
     FileData = FileHandle.readlines()
     FileHandle.close()
-    print ('DBG: Finished reading in data for file: ' + FileName)
     return FileData
 
+#Method to organize raw data.
+#   A dictionary is set up, with keys based on the first row in the CSV file.
+#   After setting up keys, values are read in and assigned to the matching key index.
+#   Data is NOT sanitized, casted or trimmed. It is simply organized in its raw format.
 def OrganizeData(FileData):
-    print('starting data organization')
     #Set up a dictionary for data fields
     DataDictionary = {}
+
+    #Utilize Python's native CSV reader to handle the quotes in the data set.
+    #This is necessary, as a simple split(',') creates more values than headers (due to legitimate commas in some fields)
     reader = csv.reader(FileData, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
     DictionaryHeaders = next(reader)
 
-    print('Prepping headers: ')
     #Prep data headers
     Headers = []
     for header in DictionaryHeaders:
         DataDictionary[header] = []
         Headers.append(header)
 
-    print('Organizing the data: ')
     #Interpret the rest of the data
     LoopLen = len(FileData) - 1
     for line in range(1, LoopLen):
@@ -43,12 +49,21 @@ def OrganizeData(FileData):
 
     return DataDictionary
 
-
-def InterpretDailyFileData(FileData):
-    print ('lol')
-
-def InterpretYearlyFileData(FileData):
-    print ('lol')
+#Method to clean data, removing incomplete ordered pairs and casting to floats.
+#Removes entries that have missing data from two given data sets.
+#Non-removed entries are casted to floats and then appended to an array to be returned.
+#	Ex: If X=2 and Y=null, this (X,Y) coordinate will be skipped.
+def CleanDataFloat(x, y):
+    floatx = []
+    floaty = []
+    
+    for i in range(0, len(x)-1):
+        if (x[i] == '' or y[i] == ''):
+            continue
+        floatx.append(float(x[i]))
+        floaty.append(float(y[i]))
+        
+    return floatx, floaty
 
 #--------------------------------------------------------------------------------
 # Helper Methods
@@ -92,25 +107,10 @@ def IsLeapYear(year):
 #--------------------------------------------------------------------------------
 # Plotting and Data Methods
 #--------------------------------------------------------------------------------
-#Removes entries that have missing data from two given data sets.
-#Non-removed entries are casted to floats and then appended to an array to be returned.
-#	Ex: If X=2 and Y=null, this (X,Y) coordinate will be skipped.
-def cleanData(x, y):
-    floatx = []
-    floaty = []
-    
-    for i in range(0, len(x)-1):
-        if (x[i] == '' or y[i] == ''):
-            continue
-        floatx.append(float(x[i]))
-        floaty.append(float(y[i]))
-        
-    return floatx, floaty
 
 #Averages a list of data for each unique key in a list of keys
 #   Example usage: A list of years (repeating keys, shuffled) with temperatures for each year entry
 def AverageDataByKey(keys, data):
-    print('DBG AverageDataByKey - START')
     #Helper Vars
     HandledKeys = []
     AverageForKey = []
@@ -133,13 +133,42 @@ def AverageDataByKey(keys, data):
     return HandledKeys, AverageForKey
 
 #Plot some data and include a trend line in one easy to use method!
-def PlotWithTrendLine(x, y):
+def PlotWithTrendLine(x, y, color='b', label='', labelLocation='upper left'):
+    #Prepare plot design choices
+    PointDesign = color + 'o'
+    TrendDesign = color + '-'
+
+    if(label != ''):
+        plot.plot(x, y, PointDesign, label=label)
+        plot.legend(loc=labelLocation)
+    else:
+        plot.plot(x, y, PointDesign)
+
+    #Generate and plot the trend line
     #Credit for this method: http://widu.tumblr.com/post/43624347354/matplotlib-trendline
-    plot.plot(x, y, 'bo')
     z = np.polyfit(x, y, 1)
     p = np.poly1d(z)
-    plot.plot(x, p(x), "b--")
+    plot.plot(x, p(x), TrendDesign)
 
+#Plot only the trend line of some data with this one easy to use method!
+def PlotTrendLineOnly(x, y, color='b', label='', labelLocation='upper left'):
+    #Prepare plot design choices
+    TrendDesign = color + '-'
+
+    #Generate trend line data
+    #Credit for this method: http://widu.tumblr.com/post/43624347354/matplotlib-trendline
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    
+    #Plot with or without label
+    if(label != ''):
+        plot.plot(x, p(x), TrendDesign, label=label)
+        plot.legend(loc=labelLocation)
+    else:
+        plot.plot(x, p(x), TrendDesign)
+    
+#--------------------------------------------------------------------------------
+# Main Script Area
 #--------------------------------------------------------------------------------
 # Global Variables
 #
@@ -154,19 +183,62 @@ YearStartDay = 1
 FileNames = {'Daily':'WeatherData-SLC-RegionDailyWeather.csv', 'Yearly':'WinterData-YearlySummaries.csv'}
 DataYearly = OrganizeData(ReadDataFile(FileNames['Yearly']))
 #DataDaily = OrganizeData(ReadDataFile(FileNames['Daily']))
-print('Data read finished, beginning cleanup and plotting...')
-
-print ('DBG: len[date]= {0}\tlen[tavg]={1}'.format(len(DataYearly['DATE']), len(DataYearly['TAVG'])))
-
-#plotScatter(DataYearly['DATE'], DataYearly['TAVG'])
-cleanedX, cleanedY = cleanData(DataYearly['DATE'], DataYearly['TAVG'])
-Year, AveTemp = AverageDataByKey(cleanedX, cleanedY)
+print('Completed reading in data sets.')
 
 #--------------------------------------------------------------------------------
-# Data Plotting
+# Data Prep and Plotting
+#   Figures are exported as individual plots, as PyPlot squishes the plots a bit too much for my taste.
 #--------------------------------------------------------------------------------
+print('Working on plot: '), #Leave trailing comma, keeps next print() on the same line.
+
+#Average Temperature per Year
+print('1 '),
+cleanedX, cleanedY = CleanDataFloat(DataYearly['DATE'], DataYearly['TAVG'])
+YearTemperatureAverage, AverageTemperature = AverageDataByKey(cleanedX, cleanedY)
+
 plot.figure()
-plot.title('Average Temperature by Year')
+plot.title('Average Temperature by Year for Salt Lake Area')
+plot.xlabel('Year')
+plot.ylabel('Average Temperature')
+PlotWithTrendLine(YearTemperatureAverage, AverageTemperature)
+plot.savefig('AverageTemperature.png')
 
-PlotWithTrendLine(Year, AveTemp)
-plot.show()
+#Average Snowfall per Year
+print('2 '),
+cleanedX, cleanedY = CleanDataFloat(DataYearly['DATE'], DataYearly['SNOW'])
+YearSnow, AverageSnow = AverageDataByKey(cleanedX, cleanedY)
+
+#plot.figure()
+plot.figure()
+plot.title('Average Snowfall by Year for Salt Lake Area')
+plot.xlabel('Year')
+plot.ylabel('Average Snowfall')
+PlotWithTrendLine(YearSnow, AverageSnow)
+plot.savefig('AverageSnow.png')
+
+#Average Snowfall per Year vs. Average Precipitation per Year
+print('3 '),
+#Snow averages were already calculated. So only Precipitation is needed.
+cleanedX, cleanedY = CleanDataFloat(DataYearly['DATE'], DataYearly['PRCP'])
+YearPrecipitation, AveragePrecipitation = AverageDataByKey(cleanedX, cleanedY)
+
+plot.figure()
+plot.title('Average Precipitation by Year for Salt Lake Area')
+plot.xlabel('Year')
+plot.ylabel('Average Precipitation')
+PlotWithTrendLine(YearPrecipitation, AveragePrecipitation, labelLocation='upper right')
+plot.tight_layout() #Prevent overlapping
+plot.savefig('AveragePrecipitation.png')
+
+#Average Snowfall per Year vs. Average Precipitation per Year
+print('4 ')
+plot.figure()
+plot.title('Average Snowfall vs. Average Precipitation')
+plot.xlabel('Year')
+plot.ylabel('Amount')
+PlotTrendLineOnly(YearSnow, AverageSnow, 'b', 'Snow', labelLocation='upper right')
+PlotTrendLineOnly(YearPrecipitation, AveragePrecipitation, 'g', 'Precipitation', labelLocation='upper right')
+plot.savefig('SnowVsPrecipitation.png', bbox_inches='tight')
+
+print('Done!\n')
+print('Finished plotting and exporting all graphs.\n')
